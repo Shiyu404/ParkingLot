2dd8a0cd92e5545570a5404710e4f66fb8958b31
Frank Yang <ffyang@student.ubc.ca>
2025-03-28 18:21:17 -0700

    Change TypeScript Setting to JavaScript Setting

---
+++ frontend/src/vite-env.js
@@ -0,0 +1,19 @@
+// 为图片文件提供模块声明
+export const svgModule = {
+  default: null
+}
+
+export const pngModule = {
+  default: null
+}
+
+export const jpgModule = {
+  default: null
+}
+
+// 导出模块声明
+export default {
+  svg: svgModule,
+  png: pngModule,
+  jpg: jpgModule
+} 
